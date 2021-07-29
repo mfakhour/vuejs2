298a4f5877be0d2b7c967982a53dd21990d7cfcb
mfakhour <47917153+mfakhour@users.noreply.github.com>
2021-07-29 16:40:19 +0100

    first-commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# vuejs2
- 
+# vue3-sidebar
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>ITSP Parameters Management Rest APIs</h1>
+    <a href="https://s2mworldwide.com/" class="link-success">click here</a>
+  </div>
+
+  <div id="app" v-for="item in data.tags" :key="item">
+        
+         <h1>{{item.name}}</h1>
+         <a href="#" class="link-success">{{item.description}}</a>
+        
+    
+  </div>
+  
+</template>
+
+<script>
+import apiData from "./api.json";
+
+export default {
+  data() {
+    return {
+      data: apiData,
+    };
+
+  },
+};
+</script>
+<style>
+
+</style>
